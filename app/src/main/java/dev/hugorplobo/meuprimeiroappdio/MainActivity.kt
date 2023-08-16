90a5581a8aad7e0619a2f83d9e13fd7db8fa94cc
Antônio Hugo <hugorplobo@gmail.com>
2023-08-16 18:39:53 -0300

    feat: add text card composable

--- app/src/main/java/dev/hugorplobo/meuprimeiroappdio/MainActivity.kt
+++ app/src/main/java/dev/hugorplobo/meuprimeiroappdio/MainActivity.kt
@@ -3,13 +3,20 @@ package dev.hugorplobo.meuprimeiroappdio
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.hugorplobo.meuprimeiroappdio.ui.theme.MeuPrimeiroAppDIOTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +29,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        TextCard(text = getString(R.string.greeting))
+                    }
                 }
             }
         }
@@ -30,17 +39,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TextCard(text: String) {
+    Card {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.padding(all = 16.dp)
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
+fun PreviewTextCard() {
     MeuPrimeiroAppDIOTheme {
-        Greeting("Android")
+        Surface(color = MaterialTheme.colorScheme.background) {
+            TextCard(text = "Hello World")
+        }
     }
 }
